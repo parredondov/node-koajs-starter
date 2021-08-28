813f5be3fe4c1c31d12f76a48afeb98944321a1e
Pablo <parredondo@eloquentcloud.com>
2021-08-27 21:36:17 -0500

    feat(knex): modify migration stub

--- knex/knex-migration-stub.js
+++ knex/knex-migration-stub.js
@@ -8,6 +8,12 @@ const TABLE_NAME = '';
  */
 async function up (knex) {
   // TODO: Define migration script
+  return knex.schema.createTable(TABLE_NAME, function (table) {
+    table.increments();
+    // TODO: Add necesary fields
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
 }
 
 /**
@@ -16,6 +22,7 @@ async function up (knex) {
  */
 async function down (knex) {
   // TODO: Define rollback script
+  return knex.schema.dropTable(TABLE_NAME);
 }
 
 module.exports = { up, down };
